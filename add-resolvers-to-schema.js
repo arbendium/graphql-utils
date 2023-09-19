0a94741646ab0a5ac773f2e1217f440e75b1d90b
Keijo Kapp <keijo.kapp@gmail.com>
2023-09-20 02:22:16 +0300

    init

---
+++ add-resolvers-to-schema.js
@@ -0,0 +1,106 @@
+import assert from 'assert';
+import { ValidationError } from 'ajv';
+import {
+	defaultFieldResolver,
+	isScalarType,
+	isObjectType,
+	getNamedType,
+	Kind,
+	print
+} from 'graphql';
+
+function parseLiteral(typeName, ast, variables) {
+	switch (ast.kind) {
+	case Kind.STRING:
+	case Kind.BOOLEAN:
+		return ast.value;
+	case Kind.INT:
+	case Kind.FLOAT:
+		return parseFloat(ast.value);
+	case Kind.OBJECT: {
+		const value = Object.create(null);
+		ast.fields.forEach(field => {
+			value[field.name.value] = parseLiteral(typeName, field.value, variables);
+		});
+
+		return value;
+	}
+	case Kind.LIST:
+		return ast.values.map(n => parseLiteral(typeName, n, variables));
+	case Kind.NULL:
+		return null;
+	case Kind.VARIABLE:
+		return variables ? variables[ast.name.value] : undefined;
+	default:
+		throw new TypeError(`${typeName} cannot represent value: ${print(ast)}`);
+	}
+}
+
+function createValidator(validator, schema) {
+	const validate = validator.compile(schema);
+
+	return value => {
+		if (!validate(value)) {
+			const e = new ValidationError(validate.errors);
+			e.message = 'Type value validation failed';
+			throw e;
+		}
+
+		return value;
+	};
+}
+
+export default function addResolversToSchema({
+	resolvers,
+	schema,
+	validator,
+	wrapFieldResolver
+}) {
+	Object.values(schema.getTypeMap())
+		.filter(type => !getNamedType(type).name.startsWith('__'))
+		.forEach(type => {
+			const typeName = type.name;
+
+			if (isScalarType(type)) {
+				if (typeName in resolvers) {
+					const schema = resolvers[typeName];
+
+					const validate = createValidator(validator, schema);
+
+					type.description = `\`\`\`json\n${JSON.stringify(schema, null, 2)}\n\`\`\``;
+					type.serialize = validate;
+					type.parseValue = validate;
+					type.parseLiteral = (ast, variables) => validate(parseLiteral(typeName, ast, variables));
+				}
+			} else if (isObjectType(type)) {
+				const fields = type.getFields();
+
+				if (typeName in resolvers) {
+					const resolverValue = resolvers[typeName];
+
+					for (const fieldName in resolverValue) {
+						const field = fields[fieldName];
+
+						assert(field != null, `Field ${typeName}.${fieldName} not defined in schema`);
+
+						const fieldResolve = resolverValue[fieldName];
+						if (typeof fieldResolve === 'function') {
+							field.resolve = wrapFieldResolver(fieldResolve);
+						} else {
+							Object.assign(field, fieldResolve);
+						}
+					}
+				}
+
+				for (const fieldName in fields) {
+					const field = fields[fieldName];
+
+					if (field.resolve == null) {
+						field.resolve = wrapFieldResolver(defaultFieldResolver);
+					}
+				}
+			}
+		});
+
+	return schema;
+}
---
+++ prepare-graphql-document.js
@@ -0,0 +1,29 @@
+import { parse, specifiedRules, validate } from 'graphql';
+
+export default function prepareGraphqlDocument({
+	document,
+	execute,
+	schema
+}) {
+	const documentAst = parse(document);
+
+	const validationErrors = validate(schema, documentAst, specifiedRules);
+
+	if (validationErrors.length > 0) {
+		throw validationErrors;
+	}
+
+	return (variables, context) => execute({
+		schema,
+		document: documentAst,
+		variableValues: variables,
+		contextValue: context
+	})
+		.then(result => {
+			if (result.errors) {
+				throw result.errors;
+			}
+
+			return result.data;
+		});
+}
